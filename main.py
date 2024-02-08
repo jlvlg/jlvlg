0f3115e915305439f9fa09530aafec664dd9e281
Lucas Vinicius <j.lucasvinicius03@gmail.com>
2024-02-08 10:46:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -141,18 +141,18 @@ readme_file_content = rf"""<div align="justify">
       /></a>
     </td>
     <td>
-      <a href="https://github.com/jlvlg/IP-Projeto-C"
+      <a href="https://github.com/jlvlg/forever-homes-shelter"
         ><img
-          src="https://github-readme-stats.vercel.app/api/pin/?username=jlvlg&repo=IP-Projeto-C"
+          src="https://github-readme-stats.vercel.app/api/pin/?username=jlvlg&repo=forever-homes-shelter"
           alt=""
       /></a>
     </td>
   </tr>
   <tr>
     <td>
-      <a href="https://github.com/jlvlg/IP-Projeto-Python"
+      <a href="https://github.com/jlvlg/Compass-Video"
         ><img
-          src="https://github-readme-stats.vercel.app/api/pin/?username=jlvlg&repo=IP-Projeto-Python"
+          src="https://github-readme-stats.vercel.app/api/pin/?username=jlvlg&repo=Compass-Video"
           alt=""
       /></a>
     </td>
@@ -166,9 +166,9 @@ readme_file_content = rf"""<div align="justify">
   </tr>
   <tr>
     <td>
-      <a href="https://github.com/jlvlg/reserva-ai"
+      <a href="https://github.com/jlvlg/social-compass"
         ><img
-          src="https://github-readme-stats.vercel.app/api/pin/?username=jlvlg&repo=reserva-ai"
+          src="https://github-readme-stats.vercel.app/api/pin/?username=jlvlg&repo=social-compass"
           alt=""
       /></a>
     </td>
